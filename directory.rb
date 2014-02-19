88cf26d312ba528861899478f84518976ace7968
Andrew <aj.cumine@btinternet.com>
2014-02-19 17:10:23 +0000

    changed directory.rb print names using while loop

--- directory.rb
+++ directory.rb
@@ -1,36 +1,31 @@
 # let's put all student's into an array
-#students = [
-#   {:name => "Mario Gintili", :cohort => :february},
-#   {:name => "Mikhail Dubov", :cohort => :february},
-#   {:name => "Karolis Noreika", :cohort => :february},
-#   {:name => "Michael Sidon", :cohort => :february},
-#   {:name => "Charles De Barros", :cohort => :february},
-#   {:name => "Ruslan Vikhor", :cohort => :february},
-#   {:name => "Toby Retallick", :cohort => :february},
-#   {:name => "Mark Mekhaiel", :cohort => :february},
-#   {:name => "Sarah Young", :cohort => :february},
-#   {:name => "Hannah Wight", :cohort => :february},
-#   {:name => "Khushkaran Singh", :cohort => :february},
-#   {:name => "Rick brunstedt", :cohort => :february},
-#   {:name => "Manjit Singh", :cohort => :february},
-#   {:name => "Alex Gaudiosi", :cohort => :february},
-#   {:name => "Ross Hepburn", :cohort => :february},
-#   {:name => "Natascia Marchese", :cohort => :february},
-#   {:name => "Tiffanie Chia", :cohort => :february},
-#   {:name => "Matthew Thomas", :cohort => :february},
-#   {:name => "Freddy McGroarty", :cohort => :february},
-#   {:name => "Tyler Rollins", :cohort => :february},
-#   {:name => "Richard Curteis", :cohort => :february},
-#   {:name => "Anna Yanova", :cohort => :february},
-#   {:name => "Andrew Cumine",:cohort => :february}
-#]
+students = [
+   {:name => "Mario Gintili", :cohort => :february},
+   {:name => "Mikhail Dubov", :cohort => :february},
+   {:name => "Karolis Noreika", :cohort => :february},
+   {:name => "Michael Sidon", :cohort => :february},
+   {:name => "Charles De Barros", :cohort => :february},
+   {:name => "Ruslan Vikhor", :cohort => :february},
+   {:name => "Toby Retallick", :cohort => :february},
+   {:name => "Mark Mekhaiel", :cohort => :february},
+   {:name => "Sarah Young", :cohort => :february},
+   {:name => "Hannah Wight", :cohort => :february},
+   {:name => "Khushkaran Singh", :cohort => :february},
+   {:name => "Rick brunstedt", :cohort => :february},
+   {:name => "Manjit Singh", :cohort => :february},
+   {:name => "Alex Gaudiosi", :cohort => :february},
+   {:name => "Ross Hepburn", :cohort => :february},
+   {:name => "Natascia Marchese", :cohort => :february},
+   {:name => "Tiffanie Chia", :cohort => :february},
+   {:name => "Matthew Thomas", :cohort => :february},
+   {:name => "Freddy McGroarty", :cohort => :february},
+   {:name => "Tyler Rollins", :cohort => :february},
+   {:name => "Richard Curteis", :cohort => :february},
+   {:name => "Anna Yanova", :cohort => :february},
+   {:name => "Andrew Cumine",:cohort => :february}
+]
 
-
-## nothing happens until we call the methods
-#print_header
-#print(students)
-#print_footer(students)
-
+=begin
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -49,25 +44,40 @@ def input_students
   #return the array of students
   students
 end
+=end
 
 def print_header
   puts "The students of my cohort at Makers Academy"
   puts "-------------"
 end
 
+=begin
 def print(students)
   students.each_with_index do |student, index|
-    if student[:name].length < 12
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+=end
+
+
+def print(students)
+  i = 0
+  student = {}
+  while i < students.length
+    student = students[i]
+    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    i += 1
+  end
+end
+
+  
+
 
 def print_footer(names)
   puts "Overall, we have #{names.length} great students"
 end
 
-students = input_students
+#students = input_students
 print_header
 print(students)
 print_footer(students)
