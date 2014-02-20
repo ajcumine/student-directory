118279ea8dc06633d36675f6c5b4194a428f4e1f
Andrew <aj.cumine@btinternet.com>
2014-02-20 16:56:10 +0000

    refactored by removing obsolete, commented code

--- directory.rb
+++ directory.rb
@@ -25,41 +25,6 @@ def interactive_menu
 end
 
 
-
-
-=begin
-# let's put all student's into an array
-
-students = [
-   {:name => "Mario Gintili", :cohort => :february},
-   {:name => "Mikhail Dubov", :cohort => :february},
-   {:name => "Karolis Noreika", :cohort => :february},
-   {:name => "Michael Sidon", :cohort => :february},
-   {:name => "Charles De Barros", :cohort => :february},
-   {:name => "Ruslan Vikhor", :cohort => :february},
-   {:name => "Toby Retallick", :cohort => :february},
-   {:name => "Mark Mekhaiel", :cohort => :february},
-   {:name => "Sarah Young", :cohort => :february},
-   {:name => "Hannah Wight", :cohort => :february},
-   {:name => "Khushkaran Singh", :cohort => :february},
-   {:name => "Rick brunstedt", :cohort => :february},
-   {:name => "Manjit Singh", :cohort => :february},
-   {:name => "Alex Gaudiosi", :cohort => :february},
-   {:name => "Ross Hepburn", :cohort => :february},
-   {:name => "Natascia Marchese", :cohort => :february},
-   {:name => "Tiffanie Chia", :cohort => :february},
-   {:name => "Matthew Thomas", :cohort => :february},
-   {:name => "Freddy McGroarty", :cohort => :february},
-   {:name => "Tyler Rollins", :cohort => :february},
-   {:name => "Richard Curteis", :cohort => :february},
-   {:name => "Anna Yanova", :cohort => :february},
-   {:name => "Andrew Cumine",:cohort => :february}
-]
-=end
-
-
-
-
 def input_students
   puts "Please enter the names of the students and their cohort".center(200)
   puts "To finish, just hit return twice".center(200)
@@ -106,31 +71,6 @@ def print_students(students)
 end
 
 
-
-=begin
-def print_students_by_cohort(students)
-  puts "-------------".center(200)
-  puts "The students sorted by cohort".center(200)
-  sorted = students.sort {|a,b| a[:cohort] <=> b[:cohort]} #sort by cohort
-  sorted.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(200)
-  end
-end
-=end
-
-
-=begin
-def print(students)
-  i = 0
-  while i < students.length
-    student = students[i]
-    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(200)
-    i += 1
-  end
-end
-=end
-
-
 def print_footer(names)
   if names.length == 1
     puts "Overall, we have 1 great student".center(200)
@@ -139,13 +79,5 @@ def print_footer(names)
   end
 end
 
-
-=begin
-students = input_students
-print_header
-print_students(students)
-print_students_by_cohort(students)
-print_footer(students)
-=end
 
 interactive_menu
