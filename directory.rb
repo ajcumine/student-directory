f31e1145ac900a37fc79f984632f990657a8157d
Andrew <aj.cumine@btinternet.com>
2014-02-20 17:34:16 +0000

    added global variable @students and replaced local variable to enable functionality

--- directory.rb
+++ directory.rb
@@ -1,21 +1,24 @@
+
+
+### NEED TO UPDATE CODE TO USE GLOBAL VARIABLE @students
+
+
+
+@students = [] # an empty array accessible to all methods
+
 def interactive_menu
-  students = []
   loop do
     # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+    print_menu
     # 2. read the input and save it into a variable
     selection = gets.chomp
     # 3. do what the user has asked
     case selection
     when "1"
-      students = input_students # input the students
+      @students = input_students # input the students
     when "2"
       # show the students
-      print_header
-      print_students(students)
-      print_footer(students)
+      show_students
     when "9"
       exit # this will terminate the program
     else
@@ -28,8 +31,6 @@ end
 def input_students
   puts "Please enter the names of the students and their cohort".center(200)
   puts "To finish, just hit return twice".center(200)
-  #create an empty array
-  students = []
   print "Student name: "
   #get the first name
   name = gets.chomp
@@ -43,14 +44,14 @@ def input_students
       month = "default"
     end
     # add the student hash to the array
-    students << {:name => name, :cohort => month}
-    puts "Now we have #{students.length} students"
+    @students << {:name => name, :cohort => month}
+    puts "Now we have #{@students.length} students"
     # get another name from the user
     print "Student name: "
     name = gets.chomp
   end
   #return the array of students
-  students
+  @students
 end
 
 
@@ -60,24 +61,35 @@ def print_header
 end
 
 
-def print_students(students)
-  if students.length == 0
+def print_students
+  if @students.length == 0
     puts "There are no students".center(200)
   else
-    students.each_with_index do |student, index|
+    @students.each_with_index do |student, index|
       puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(200)
     end
   end
 end
 
 
-def print_footer(names)
-  if names.length == 1
+def print_footer
+  if @students.length == 1
     puts "Overall, we have 1 great student".center(200)
   else
-    puts "Overall, we have #{names.length} great students".center(200)
+    puts "Overall, we have #{@students.length} great students".center(200)
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we will be adding more items
+end
+
+def show_students
+  print_header
+  print_students
+  print_footer
+end
 
 interactive_menu
