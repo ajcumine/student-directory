a8efb79b9224dc5e6121e0f8ec1faabd44e329bf
Andrew <aj.cumine@btinternet.com>
2014-02-25 14:51:26 +0000

    refactor to close File automatically using File do block

--- directory.rb
+++ directory.rb
@@ -82,23 +82,23 @@ end
 
 
 def save_students
-  file = File.open("students.csv", "w") # open file for writing
-  @students.each do |student| # iterate over the array of students
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  File.open("students.csv", "w") do |file|# open file for writing
+    @students.each do |student| # iterate over the array of students
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
   end
-  file.close
 end
 
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-    add_student(name, cohort)
+  File.open(filename, "r") do |file|
+    file.readlines.each do |line|
+      name, cohort = line.chomp.split(',')
+      add_student(name, cohort)
+    end
   end
-  file.close
 end
 
 
